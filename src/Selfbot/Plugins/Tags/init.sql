ad0f2eb88654f5d9676a57e71f5d9fd6f330f7ea
Hayden Andreyka <haydenandreyka@gmail.com>
2017-08-17 21:05:00 -0700

    fix up some SQL

--- src/Selfbot/Plugins/Tags/init.sql
+++ src/Selfbot/Plugins/Tags/init.sql
@@ -3,7 +3,6 @@
 CREATE TABLE IF NOT EXISTS `tags` (
 	`id` INT NOT NULL AUTO_INCREMENT,	-- unique id for each tag
 	`title` VARCHAR(50) UNIQUE,	-- title of tag
-	`text` TEXT,	-- tag content
-	PRIMARY KEY (`id`),
-	INDEX (`title`)
+	`text` TEXT NULL COLLATE 'utf8mb4_bin',	-- tag content
+	PRIMARY KEY (`id`)
 );
